ebe9b568edfa7c624d81cbe5a9c33e5066ca1278
Tomy8s <tomy8s@hotmail.com>
2016-11-28 15:47:06 +0000

    Removed obsolete test

--- spec/features/features_spec.rb
+++ spec/features/features_spec.rb
@@ -1,17 +1,10 @@
 require 'spec_helper'
 
 describe Database do
-  context 'submitting to /set?' do
-    scenario 'redirects to /get with params' do
-      visit '/set'
-      expect(current_path).to eq '/get'
-    end
-  end
-
   context 'displaying on /get?' do
     scenario 'displays key' do
       visit '/set?mykey=myval'
-      expect(page).to eq 'myval'
+      expect(page).to have_content 'myval'
     end
   end
 end
